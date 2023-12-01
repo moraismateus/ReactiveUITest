2f1ad42fd4522d3ca47a5d48b9583a26de7870c6
Mateus Morais <mateus.morais@swatmaps.com>
2023-12-01 08:48:56 -0800

    Renaming files and removing unnecessary pages

--- Services/TestService.cs
+++ Services/TestService.cs
@@ -2,12 +2,12 @@ using System.Reactive.Subjects;
 
 public class TestService
 {
-        private readonly Subject<string> _dataChangedSubject = new Subject<string>();
+    private readonly Subject<string> _dataChangedSubject = new Subject<string>();
 
-        public IObservable<string> DataChanged => _dataChangedSubject;
+    public IObservable<string> DataChanged => _dataChangedSubject;
 
-        public void OnDataChanged(string data)
-        {
-                _dataChangedSubject.OnNext(data);
-        }
+    public void OnDataChanged(string data)
+    {
+        _dataChangedSubject.OnNext(data);
+    }
 }
