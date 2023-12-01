8e8c51cabb481a1300285c7c8357f91c28ae2a36
Mateus Morais <mateus.morais@swatmaps.com>
2023-12-01 08:42:35 -0800

    Initial Commit - RxUI working.

---
+++ Services/TestService.cs
@@ -0,0 +1,13 @@
+using System.Reactive.Subjects;
+
+public class TestService
+{
+        private readonly Subject<string> _dataChangedSubject = new Subject<string>();
+
+        public IObservable<string> DataChanged => _dataChangedSubject;
+
+        public void OnDataChanged(string data)
+        {
+                _dataChangedSubject.OnNext(data);
+        }
+}
---
+++ ViewModels/TestViewModel.cs
@@ -0,0 +1,45 @@
+using ReactiveUI;
+using System;
+using System.Reactive;
+using System.Reactive.Linq;
+
+public class TestViewModel : ReactiveObject
+{
+    private readonly TestService _testService;
+    public ReactiveCommand<string, Unit> SomeCommand { get; }
+
+    private string _customText = "";
+    public string CustomText
+    {
+        get => _customText;
+        set => this.RaiseAndSetIfChanged(ref _customText, value);
+    }
+
+    private bool _showAlert;
+    public bool ShowAlert
+    {
+        get => _showAlert;
+        set => this.RaiseAndSetIfChanged(ref _showAlert, value);
+    }
+
+    public TestViewModel(TestService testService)
+    {
+        _testService = testService;
+
+        _testService.DataChanged
+            .Subscribe(data =>
+            {
+                CustomText = data;
+                ShowAlert = !_showAlert;
+            });
+
+        SomeCommand = ReactiveCommand.Create<string, Unit>(ExecuteCommand);
+    }
+
+    private Unit ExecuteCommand(string newData)
+    {
+        _testService.OnDataChanged(newData);
+
+        return Unit.Default;
+    }
+}
